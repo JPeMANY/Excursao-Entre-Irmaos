3f4416cc16618715424ab00eb60480606d07c2ba
JPeMANY <jp.hobuss@gmail.com>
2024-08-05 22:17:49 -0300

    Alterações

---
+++ scripts/dados.js
@@ -0,0 +1,11 @@
+const destino1 = 'Cataratas do Iguaçu';
+
+const destino2 = 'Cataratas do Iguaçu';
+
+const destino3 = 'Cataratas do Iguaçu';
+
+const destino4 = 'Cataratas do Iguaçu';
+
+const destino5 = 'Cataratas do Iguaçu';
+
+const destino6 = 'Cataratas do Iguaçu';
