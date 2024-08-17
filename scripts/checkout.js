43512090b8acb303bd98f69994add7bab74dff52
JPeMANY <jp.hobuss@gmail.com>
2024-08-17 12:47:19 -0300

    Atualizações

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,14 +1,126 @@
-const cartoes = Array.from(document.querySelectorAll('.formDestinos div'))
+// ATUALIZA AS IMAGENS / NOME / VALOR / DOS CARTÕES
 
-const imagensDestinos = Array.from(document.querySelectorAll('.formDestinos img'));
+    const formDestinos = document.querySelector('.formDestinos');
+    const cartoes = document.querySelectorAll('.formDestinos label');
+    const inputCartoes = document.querySelectorAll('.formDestinos input')
+    const imagensDestinos = document.querySelectorAll('.formDestinos img');
+    const tituloCartoes = document.querySelectorAll('.formDestinos h2');
+    const valorCartoes = document.querySelectorAll('.formDestinos label span');
 
-for (let i = 0; i < imagensDestinos.length; i++) {
-    if (destinos[i].imagens != '') {
-        imagensDestinos[i].src = `imagens/destino${i+1}/${destinos[i].imagens[i]}`;
-        
-        console.log('com img')
-    } else {
-        cartoes[i].style.display = 'none';
-        console.log('sem img')
+    for (let i = 0; i < cartoes.length; i++) {
+        // Verifica se há imagens no banco de dados para a viagem
+        if (destinos[i].imagens != '') {
+            // Atualiza a imagem
+            imagensDestinos[i].src = `imagens/destino${i+1}/${destinos[i].imagens[i]}`;
+
+            // Atualiza o título / OPCIONAL replace !!!!!!!!!!!!!!!!!!!!!! Implementar no Google Sheets
+            tituloCartoes[i].textContent = destinos[i].titulo.replace(/\*/g, '');
+
+            // Atualiza o valor 
+            valorCartoes[i].textContent = 'R$ ' + destinos[i].valor.replace(/,00/g, '') + ',00';
+            inputCartoes[i].value = destinos[i].valor.replace(/,00/g, '');
+
+        } else {
+            cartoes[i].style.display = 'none';
+            console.log('sem img')
+        }
     }
-}
+
+
+// BACK
+
+    // Inputs
+    const inputs = document.querySelectorAll('.formDados input')
+    const inputNome = document.getElementById('nome');
+    const inputPessoas = document.getElementById('pessoas');
+    const inputWhats = document.getElementById('whats');
+    const inputsPagamento = document.getElementById('pagamento');
+    const inputObservacao = document.getElementById('observacao');
+
+    // Valor
+    const valorP = document.querySelector('.valorTotal');
+
+    // Botões
+    const btnLimpar = document.getElementById('btnLimpar');
+    const btnCalcular = document.getElementById('btnCalcular');
+    const btnEnviar = document.getElementById('btnEnviar');
+
+    // Campos obrigatórios
+    const camposObrigatorios = document.querySelectorAll('.campoObrigatorio');
+
+    let valorTotal;
+
+
+    function atualizarValor() {
+        const destinoSelecionado = document.querySelector('.formDestinos input:checked');
+        valorTotal = destinoSelecionado.value * inputPessoas.value;
+        valorP.textContent = `R$ ${valorTotal},00`;
+    }
+
+
+// Clique botão CALCULAR
+    btnCalcular.addEventListener('click', ()=> {
+        let verificarPreenchimento = 0;
+
+        // Verifica se alguma opção de destino está Check
+        const destinosCheck = document.querySelector('.formDestinos input:checked') != null;
+        if (destinosCheck) {
+            verificarPreenchimento += 1;
+        } else {
+            formDestinos.style.border = '3px solid red';
+            setTimeout(()=> {
+                formDestinos.style.border = 'none';
+            }, 4000)
+        }
+
+        // Verifica cada input obrigatório
+        for (let i = 0; i < (camposObrigatorios.length); i++) {
+            // Verifica se campos estão preenchidos
+            if (inputs[i].value != '') {
+                verificarPreenchimento += 1;
+            } else {
+                // Marcar vermelho
+                inputs[i].style.border = '3px solid red';
+                // Voltar ao normal
+                setTimeout(()=> { inputs[i].style.border = '1px solid #b3b3b3' }, 4000)
+            }
+        }
+        // Se todos estiverem preenchidos
+        if (verificarPreenchimento >= (camposObrigatorios + 1)) {
+            console.log('todos os campos estão preenchidos!')
+            atualizarValor();
+        } else {
+            console.log('faltam ' + (4 - verificarPreenchimento) + ' campos');
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // Formatar o número de Whats
+        inputWhats.addEventListener('input', function(e) {
+            let input = e.target.value;
+
+            input = input.replace(/\D/g, ''); // Remove tudo que não é dígito
+
+            if (input.length > 10) {
+                input = input.replace(/^(\d{2})(\d{5})(\d{4}).*/, '($1) $2-$3');
+            } else if (input.length > 6) {
+                input = input.replace(/^(\d{2})(\d{4})(\d{0,4}).*/, '($1) $2-$3');
+            } else if (input.length > 2) {
+                input = input.replace(/^(\d{2})(\d{0,5})/, '($1) $2');
+            } else {
+                input = input.replace(/^(\d*)/, '($1');
+            }
+
+            e.target.value = input;
+        });
--- scripts/dados.js
+++ scripts/dados.js
@@ -4,6 +4,7 @@ const destinos = [
         destinos: 'Bento Gonçalves / Carlos Barbosa / Garibaldi',
         dataInicio: '11/08',
         dataRetorno: '11/08',
+        valor: '468,00',
         imagens: [
             '1.jpg',
             '2.png',
@@ -17,6 +18,7 @@ const destinos = [
         destinos: 'Buenos Aires / Betel / Tigre / Campanópolis',
         dataInicio: '16/08 ',
         dataRetorno: '21/08',
+        valor: '7503,00',
         imagens: [
             '1.jpg',
             '2.png',
@@ -30,6 +32,7 @@ const destinos = [
         destinos: '',
         dataInicio: '',
         dataRetorno: '',
+        valor: '',
         imagens: []
     },
     {
@@ -37,6 +40,7 @@ const destinos = [
         destinos: 'Maria Fumaça / Epopeia Italiana',
         dataInicio: '01/09',
         dataRetorno: '01/09',
+        valor: '650,00',
         imagens: [
             '1.jpg',
             '2.png',
@@ -50,6 +54,7 @@ const destinos = [
         destinos: 'Termas Marcelino Ramos ',
         dataInicio: '06/09',
         dataRetorno: '08/09',
+        valor: '920,00',
         imagens: [
             '1.jpg',
             '2.png',
@@ -63,6 +68,7 @@ const destinos = [
         destinos: '',
         dataInicio: '',
         dataRetorno: '',
+        valor: '',
         imagens: []
     },
     {
@@ -70,6 +76,7 @@ const destinos = [
         destinos: '',
         dataInicio: '',
         dataRetorno: '',
+        valor: '',
         imagens: []
     },
     {
@@ -77,6 +84,7 @@ const destinos = [
         destinos: '',
         dataInicio: '',
         dataRetorno: '',
+        valor: '',
         imagens: []
     },
     {
@@ -84,6 +92,7 @@ const destinos = [
         destinos: '',
         dataInicio: '',
         dataRetorno: '',
+        valor: '',
         imagens: []
     }
 ];
