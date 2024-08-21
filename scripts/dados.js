c8a77ef5e3d659612b0ee42f3464e84c2de435d7
JPeMANY <116897201+JPeMANY@users.noreply.github.com>
2024-08-21 16:04:50 -0300

    Atualizando o conteúdo do arquivo

--- scripts/dados.js
+++ scripts/dados.js
@@ -1,94 +1,94 @@
 
   const destinos = [
     {
-        titulo: 'Viagem Gastronômica',
-        destinos: 'Bento Gonçalves / Carlos Barbosa / Garibaldi',
-        descricao: '',
-        dataInicio: '08/11/2024',
-        dataRetorno: '08/11/2024',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['535938745.jpg']
-      },{
-        titulo: '',
-        destinos: 'Buenos Aires / Betel / Tigre / Campanópolis ',
-        descricao: '',
-        dataInicio: 'NaN/NaN/NaN',
-        dataRetorno: 'NaN/NaN/NaN',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['']
-      },{
-        titulo: '',
-        destinos: '',
-        descricao: '',
-        dataInicio: 'NaN/NaN/NaN',
-        dataRetorno: 'NaN/NaN/NaN',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['maria-fumaca-bento-goncalves.jpg','unnamed.jpg']
-      },{
-        titulo: '',
-        destinos: 'Maria Fumaça / Epopeia Italiana ',
-        descricao: '',
-        dataInicio: '09/01/2024',
-        dataRetorno: '09/01/2024',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['']
-      },{
-        titulo: '',
-        destinos: 'Termas Marcelino Ramos ',
-        descricao: '',
-        dataInicio: '09/06/2024',
-        dataRetorno: '09/08/2024',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['']
-      },{
-        titulo: '',
-        destinos: '',
-        descricao: '',
-        dataInicio: 'NaN/NaN/NaN',
-        dataRetorno: 'NaN/NaN/NaN',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['']
-      },{
-        titulo: '',
-        destinos: '',
-        descricao: '',
-        dataInicio: 'NaN/NaN/NaN',
-        dataRetorno: 'NaN/NaN/NaN',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['']
-      },{
-        titulo: '',
-        destinos: '',
-        descricao: '',
-        dataInicio: 'NaN/NaN/NaN',
-        dataRetorno: 'NaN/NaN/NaN',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['']
-      },{
-        titulo: '',
-        destinos: '',
-        descricao: '',
-        dataInicio: 'NaN/NaN/NaN',
-        dataRetorno: 'NaN/NaN/NaN',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['']
-      },{
-        titulo: '',
-        destinos: '',
-        descricao: '',
-        dataInicio: 'NaN/NaN/NaN',
-        dataRetorno: 'NaN/NaN/NaN',
-        lugaresDisponiveis: '',
-        valor: '',
-        imagens: ['']
-      },
+      titulo: 'Viagem Gastronômica',
+      destinos: 'Bento Gonçalves / Carlos Barbosa / Garibaldi',
+      descricao: '',
+      dataInicio: '08/11/2024',
+      dataRetorno: '08/11/2024',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['535938745.jpg']
+    },{
+      titulo: '',
+      destinos: 'Buenos Aires / Betel / Tigre / Campanópolis ',
+      descricao: '',
+      dataInicio: '16/08/2024',
+      dataRetorno: '21/08/2024',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['']
+    },{
+      titulo: '',
+      destinos: '',
+      descricao: '',
+      dataInicio: 'NaN/NaN/NaN',
+      dataRetorno: 'NaN/NaN/NaN',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['maria-fumaca-bento-goncalves.jpg','unnamed.jpg']
+    },{
+      titulo: '',
+      destinos: 'Maria Fumaça / Epopeia Italiana ',
+      descricao: '',
+      dataInicio: '09/01/2024',
+      dataRetorno: '09/01/2024',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['']
+    },{
+      titulo: '',
+      destinos: 'Termas Marcelino Ramos ',
+      descricao: '',
+      dataInicio: '09/06/2024',
+      dataRetorno: '09/08/2024',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['']
+    },{
+      titulo: '',
+      destinos: '',
+      descricao: '',
+      dataInicio: 'NaN/NaN/NaN',
+      dataRetorno: 'NaN/NaN/NaN',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['']
+    },{
+      titulo: '',
+      destinos: '',
+      descricao: '',
+      dataInicio: 'NaN/NaN/NaN',
+      dataRetorno: 'NaN/NaN/NaN',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['']
+    },{
+      titulo: '',
+      destinos: '',
+      descricao: '',
+      dataInicio: 'NaN/NaN/NaN',
+      dataRetorno: 'NaN/NaN/NaN',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['']
+    },{
+      titulo: '',
+      destinos: '',
+      descricao: '',
+      dataInicio: 'NaN/NaN/NaN',
+      dataRetorno: 'NaN/NaN/NaN',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['']
+    },{
+      titulo: '',
+      destinos: '',
+      descricao: '',
+      dataInicio: 'NaN/NaN/NaN',
+      dataRetorno: 'NaN/NaN/NaN',
+      lugaresDisponiveis: '',
+      valor: '',
+      imagens: ['']
+    },
   ]
