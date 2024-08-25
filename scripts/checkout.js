a3bb58bb79aaddab34ce7f387d46107b610fe96d
JPeMANY <jp.hobuss@gmail.com>
2024-08-25 11:57:49 -0300

    Atualizações

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,172 +1,116 @@
-// ATUALIZA AS IMAGENS / NOME / VALOR / DOS CARTÕES
+// VARIÁVEIS
+    const form = document.getElementById('formReserva');
 
-    const infoDestino = document.querySelector('.infoDestino');
+    const inputWhats = document.getElementById('whats');
+    const inputNascimento = document.getElementById('nascimento');
+
+    const inputsPagamento = document.getElementById('pagamento');
+    const inputPessoas = document.getElementById('pessoas');
+
     const cartoes = document.querySelectorAll('.infoDestino label');
     const inputCartoes = document.querySelectorAll('.infoDestino input')
     const imagensDestinos = document.querySelectorAll('.infoDestino img');
     const tituloCartoes = document.querySelectorAll('.infoDestino h2');
     const valorCartoes = document.querySelectorAll('.infoDestino label span');
 
-    for (let i = 0; i < cartoes.length; i++) {
-        // Verifica se há imagens no banco de dados para a viagem
-        if (destinos[i].imagens != '') {
-            // Atualiza a imagem
-            imagensDestinos[i].src = `imagens/destino${i+1}/${destinos[i].imagens[i]}`;
+    const valorTotalSpan = document.getElementById('valorTotal');
 
-            // Atualiza o título / OPCIONAL replace !!!!!!!!!!!!!!!!!!!!!! Implementar no Google Sheets
-            tituloCartoes[i].textContent = destinos[i].titulo.replace(/\*/g, '');
-
-            // Atualiza o valor 
-            valorCartoes[i].textContent = 'R$ ' + destinos[i].valor.replace(/,00/g, '') + ',00';
-            inputCartoes[i].value = destinos[i].valor.replace(/,00/g, '');
-
-        } else {
-            cartoes[i].style.display = 'none';
-            console.log('sem img')
-        }
-    }
-
-
-// BACK
-
-    // Inputs
-    const inputs = document.querySelectorAll('.formDados input')
-    const inputNome = document.getElementById('nome');
-    const inputPessoas = document.getElementById('pessoas');
-    const inputWhats = document.getElementById('whats');
-    const inputsPagamento = document.getElementById('pagamento');
-    const inputObservacao = document.getElementById('observacao');
-
-    // Valor
-    const valorP = document.querySelector('.valorTotal');
-
-    // Botões
-    const btnLimpar = document.getElementById('btnLimpar');
-    const btnCalcular = document.getElementById('btnCalcular');
-    const btnEnviar = document.getElementById('btnEnviar');
-
-    // Campos obrigatórios
     const camposObrigatorios = document.querySelectorAll('.campoObrigatorio');
-
-    let valorTotal;
-
-    function atualizarValor() {
-        const destinoSelecionado = document.querySelector('.infoDestino input:checked');
-        valorTotal = destinoSelecionado.value * inputPessoas.value;
-        valorP.textContent = `R$ ${valorTotal},00`;
-    }
+    // ---- ADICIONAR DESTAQUES PARA CAMPOS NÃO PREENCHIDOS
 
 
-// Clique botão CALCULAR
-    btnCalcular.addEventListener('click', ()=> {
-        let verificarPreenchimento = 0;
+// ATUALIZAR AS IMAGENS / NOME / VALOR / DOS CARTÕES
+    function atualizarDadosDestino (atualizarValue) {{
+        for (let i = 0; i < cartoes.length; i++) {
+            if (destinos[i].titulo != '') { // Se houver título no banco de dados
+                const nomeViagem = destinos[i].titulo;
+                const valorViagem = `R$ ${destinos[i].valor},00`;
 
-        // Verifica se alguma opção de destino está Check
-        const destinosCheck = document.querySelector('.infoDestino input:checked') != null;
-        if (destinosCheck) {
-            verificarPreenchimento += 1;
-        } else {
-            infoDestino.style.border = '3px solid red';
-            setTimeout(()=> {
-                infoDestino.style.border = 'none';
-            }, 4000)
-        }
+                tituloCartoes[i].textContent = nomeViagem;
+                valorCartoes[i].textContent = valorViagem;
 
-        // Verifica cada input obrigatório
-        for (let i = 0; i < (camposObrigatorios.length); i++) {
-            // Verifica se campos estão preenchidos
-            if (inputs[i].value != '') {
-                verificarPreenchimento += 1;
+                inputCartoes[i].value = destinos[i].valor;
+
+                // Assim que enviar, troca os values, para garantir que não haverá hacks de alteração
+                if (atualizarValue) {
+                    inputCartoes[i].value = `${nomeViagem} (${valorViagem})`;
+                }
             } else {
-                // Marcar vermelho
-                inputs[i].style.border = '3px solid red';
-                // Voltar ao normal
-                setTimeout(()=> { inputs[i].style.border = '1px solid #b3b3b3' }, 4000)
+                // Oculta se não houverem informações de título
+                cartoes[i].style.display = 'none';
             }
         }
+    }}
+    atualizarDadosDestino(false);
 
-        console.log(verificarPreenchimento)
 
-        // Se todos estiverem preenchidos
-        if (verificarPreenchimento >= (camposObrigatorios.length + 1)) {
-            console.log('todos os campos estão preenchidos!')
-            atualizarValor();
-        } else {
-            console.log('faltam ' + (5 - verificarPreenchimento) + ' campos');
-        }
+// ATUALIZAR VALOR TOTAL
+// Se o usuário atualizar as informações, o valor é atualizado
+    inputCartoes.forEach(opcaoDestino => {
+        opcaoDestino.addEventListener('click', () => {
+            calcularValor();
+        })
     })
 
+    inputPessoas.addEventListener('input', () => {
+        calcularValor();
+    })
+
+// Função para calcular o valor
+    function calcularValor() {
+        const destinoSelecionado = document.querySelector('.infoDestino input:checked');
+
+        // Se houver um elemento input:checked
+        if (destinoSelecionado != null) {
+            let valorTotal = Number(destinoSelecionado.value) * Number(inputPessoas.value);
+            valorTotalSpan.textContent = valorTotal;
+        }
+    }
 
 
+// FORMATAÇÕES
+// Formatar o número de Whats
+    inputWhats.addEventListener('input', function(e) {
+        let input = e.target.value;
 
+        input = input.replace(/\D/g, ''); // Remove tudo que não é dígito
 
+        if (input.length > 10) {
+            input = input.replace(/^(\d{2})(\d{5})(\d{4}).*/, '($1) $2-$3');
+        } else if (input.length > 6) {
+            input = input.replace(/^(\d{2})(\d{4})(\d{0,4}).*/, '($1) $2-$3');
+        } else if (input.length > 2) {
+            input = input.replace(/^(\d{2})(\d{0,5})/, '($1) $2');
+        } else {
+            input = input.replace(/^(\d*)/, '($1');
+        }
+        e.target.value = input;
+    });
 
+// Define o input como tipo DATA (evitar que o span suba antes do usuário clicar)
+    inputNascimento.addEventListener('click', e => {
+        e.target.type = 'date';
+    })
 
-
-
-
-
-
+// CÓDIGO PARA ENVIO NO GOOGLE SHEETS
+    form.addEventListener('submit', function(e) {
+        atualizarDadosDestino(true);
+        e.preventDefault(); // Impede de enviar o formulário
+        // ADICIONAR TELA DE ESPERA! ---------------------------------------
     
-    // Formatar o número de Whats
-        inputWhats.addEventListener('input', function(e) {
-            let input = e.target.value;
-
-            input = input.replace(/\D/g, ''); // Remove tudo que não é dígito
-
-            if (input.length > 10) {
-                input = input.replace(/^(\d{2})(\d{5})(\d{4}).*/, '($1) $2-$3');
-            } else if (input.length > 6) {
-                input = input.replace(/^(\d{2})(\d{4})(\d{0,4}).*/, '($1) $2-$3');
-            } else if (input.length > 2) {
-                input = input.replace(/^(\d{2})(\d{0,5})/, '($1) $2');
-            } else {
-                input = input.replace(/^(\d*)/, '($1');
-            }
-
-            e.target.value = input;
-        });
-
-
-// Botões página
-
-        //buttonCima
-        //buttonBaixo
-
-        const infoPessoais = document.querySelector('.infoPessoais');
-        //const infoDestino = document.querySelector('.infoDestino'); - JÁ EXISTE
-        const infoViagem = document.querySelector('.infoViagem');
-        const revisao = document.querySelector('.revisao');
-
-        const btnAnterior = document.getElementById('btnAnterior');
-        const btnProximo = document.getElementById('btnProximo');
-
-
-
-
-
-
-
-
-        // ENVIAR
-
-        const form = document.getElementById('formReserva');
-
-        form.addEventListener('submit', function(e) {
-          e.preventDefault();
-      
-          const formData = new FormData(form);
-      
-          fetch('https://script.google.com/macros/s/AKfycbyTo1VvFzArzFzNRhKytllZdscQnhQue2iU09OS0PrS8L2czM-7EWqt2flNpcn3WOjx/exec', {
+        const formData = new FormData(form);
+    
+        fetch('https://script.google.com/macros/s/AKfycbyTo1VvFzArzFzNRhKytllZdscQnhQue2iU09OS0PrS8L2czM-7EWqt2flNpcn3WOjx/exec', {
             method: 'POST',
             body: formData,
-          })
-          .then(response => response.text())
-          .then(result => {
+        })
+        .then(response => response.text())
+        .then(result => {
             alert(result);
             form.reset(); // Limpa o formulário após o envio
-          })
-          .catch(error => {
+            valorTotalSpan.textContent = 0;
+        })
+        .catch(error => {
             console.error('Erro:', error);
-          });
         });
+    });
--- scripts/dados.js
+++ scripts/dados.js
@@ -1,93 +1,83 @@
-  const destinos = [
-    {
-      titulo: 'Viagem Gastronômica',
-      destinos: 'Bento Gonçalves / Carlos Barbosa / Garibaldi',
-      descricao: '',
-      dataInicio: '08/11/2024',
-      dataRetorno: '08/11/2024',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['535938745.jpg']
-    },{
-      titulo: '',
-      destinos: 'Buenos Aires / Betel / Tigre / Campanópolis ',
-      descricao: '',
-      dataInicio: '16/08/2024',
-      dataRetorno: '21/08/2024',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['']
-    },{
-      titulo: '',
-      destinos: '',
-      descricao: '',
-      dataInicio: 'NaN/NaN/NaN',
-      dataRetorno: 'NaN/NaN/NaN',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['maria-fumaca-bento-goncalves.jpg','unnamed.jpg']
-    },{
-      titulo: '',
-      destinos: 'Maria Fumaça / Epopeia Italiana ',
-      descricao: '',
-      dataInicio: '09/01/2024',
-      dataRetorno: '09/01/2024',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['']
-    },{
-      titulo: '',
-      destinos: 'Termas Marcelino Ramos ',
-      descricao: '',
-      dataInicio: '09/06/2024',
-      dataRetorno: '09/08/2024',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['']
-    },{
-      titulo: '',
-      destinos: '',
-      descricao: '',
-      dataInicio: 'NaN/NaN/NaN',
-      dataRetorno: 'NaN/NaN/NaN',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['']
-    },{
-      titulo: '',
-      destinos: '',
-      descricao: '',
-      dataInicio: 'NaN/NaN/NaN',
-      dataRetorno: 'NaN/NaN/NaN',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['']
-    },{
-      titulo: '',
-      destinos: '',
-      descricao: '',
-      dataInicio: 'NaN/NaN/NaN',
-      dataRetorno: 'NaN/NaN/NaN',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['']
-    },{
-      titulo: '',
-      destinos: '',
-      descricao: '',
-      dataInicio: 'NaN/NaN/NaN',
-      dataRetorno: 'NaN/NaN/NaN',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['']
-    },{
-      titulo: '',
-      destinos: '',
-      descricao: '',
-      dataInicio: 'NaN/NaN/NaN',
-      dataRetorno: 'NaN/NaN/NaN',
-      lugaresDisponiveis: '',
-      valor: '',
-      imagens: ['']
-    },
-  ]
+const destinos = [
+  {
+    titulo: 'Viagem Gastronômica',
+    destinos: 'Bento Gonçalves / Carlos Barbosa / Garibaldi',
+    descricao: '',
+    dataInicio: '08/11/2024',
+    dataRetorno: '08/11/2024',
+    lugaresDisponiveis: '',
+    valor: '500'
+  },{
+    titulo: 'Betel',
+    destinos: 'Buenos Aires / Betel / Tigre / Campanópolis ',
+    descricao: '',
+    dataInicio: '16/08/2024',
+    dataRetorno: '21/08/2024',
+    lugaresDisponiveis: '',
+    valor: '1450'
+  },{
+    titulo: '',
+    destinos: '',
+    descricao: '',
+    dataInicio: 'NaN/NaN/NaN',
+    dataRetorno: 'NaN/NaN/NaN',
+    lugaresDisponiveis: '',
+    valor: ''
+  },{
+    titulo: 'Maria Fumaça',
+    destinos: 'Maria Fumaça / Epopeia Italiana ',
+    descricao: '',
+    dataInicio: '09/01/2024',
+    dataRetorno: '09/01/2024',
+    lugaresDisponiveis: '',
+    valor: '400'
+  },{
+    titulo: 'Termas',
+    destinos: 'Termas Marcelino Ramos ',
+    descricao: '',
+    dataInicio: '09/06/2024',
+    dataRetorno: '09/08/2024',
+    lugaresDisponiveis: '',
+    valor: '300'
+  },{
+    titulo: '',
+    destinos: '',
+    descricao: '',
+    dataInicio: 'NaN/NaN/NaN',
+    dataRetorno: 'NaN/NaN/NaN',
+    lugaresDisponiveis: '',
+    valor: ''
+  },{
+    titulo: '',
+    destinos: '',
+    descricao: '',
+    dataInicio: 'NaN/NaN/NaN',
+    dataRetorno: 'NaN/NaN/NaN',
+    lugaresDisponiveis: '',
+    valor: ''
+  },{
+    titulo: '',
+    destinos: '',
+    descricao: '',
+    dataInicio: 'NaN/NaN/NaN',
+    dataRetorno: 'NaN/NaN/NaN',
+    lugaresDisponiveis: '',
+    valor: ''
+  },{
+    titulo: '',
+    destinos: '',
+    descricao: '',
+    dataInicio: 'NaN/NaN/NaN',
+    dataRetorno: 'NaN/NaN/NaN',
+    lugaresDisponiveis: '',
+    valor: ''
+  },{
+    titulo: '',
+    destinos: '',
+    descricao: '',
+    dataInicio: 'NaN/NaN/NaN',
+    dataRetorno: 'NaN/NaN/NaN',
+    lugaresDisponiveis: '',
+    valor: ''
+  }
+]
